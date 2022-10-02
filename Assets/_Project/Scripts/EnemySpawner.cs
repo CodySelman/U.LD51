0e5ac4e0a103498f1f0dd465d30cd6ace018cf09
CodySelman <codylselman@gmail.com>
2022-10-02 08:37:28 -0700

    cleanup

--- Assets/_Project/Scripts/EnemySpawner.cs
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -2,14 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using CodTools.Utilities;
-using Random = System.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
     public static EnemySpawner Instance;
     
-    public List<Transform> spawnTransforms = new();
     public ObjectPool<Enemy> WolfPool;
     public ObjectPool<XpDrop> XpPool;
 
@@ -21,8 +18,6 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] float spawnRateIncreaseTime = 5f;
     [SerializeField] float spawnRateInitial = 3f;
     
-    List<Vector3> spawnPositions = new();
-
     [SerializeField] float spawnRate;
     [SerializeField] float spawnRateIncreaseTimer;
     [SerializeField] float spawnTimer;
@@ -40,10 +35,6 @@ public class EnemySpawner : MonoBehaviour
         spawnRate = spawnRateInitial;
         spawnRateIncreaseTimer = spawnRateIncreaseTime;
         spawnTimer = spawnRate;
-        
-        foreach (Transform t in spawnTransforms) {
-            spawnPositions.Add(t.position);
-        }
 
         Transform trans = transform;
         WolfPool = new ObjectPool<Enemy>(wolfPrefab, 10, trans);
@@ -69,7 +60,6 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnWolfRandom() {
         Enemy e = WolfPool.Get();
-        // e.transform.position = RandomUtils.GetRandomItem(spawnPositions);
         float angle = UnityEngine.Random.Range(0f, Mathf.PI * 2);
         Vector3 dir = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0);
         Vector3 pos = dir * spawnDistance;
